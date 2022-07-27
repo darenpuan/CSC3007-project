8e966920609c45e4e4295eaa4b40a4d010227e42
Daren <darenpuan@gmail.com>
2022-07-27 14:35:09 +0800

    Integration
    Co-Authored-By: Michelle Tan - 1901861 <65145063+M1chT@users.noreply.github.com>
    Co-Authored-By: 1901854_thenuwin <70744790+1901854-thenuwin@users.noreply.github.com>

--- js/singaporeContribution.js
+++ js/singaporeContribution.js
@@ -1,172 +1,155 @@
- // set the dimensions and margins of the graph
- const margin = { top: 10, right: 30, bottom: 30, left: 60 },
- width1 = 460 - margin.left - margin.right,
- height1 = 400 - margin.top - margin.bottom;
+let filteredSG;
+    let descSG = {};
+    var myChart;
+    var color = {};
 
-// append the svg object to the body of the page
-const svg1 = d3
- .select("#lineChart")
- .append("svg")
- .attr("width", width1 + margin.left + margin.right)
- .attr("height", height1 + margin.top + margin.bottom)
- .append("g")
- .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    function filterResult(query) {
+      
+        if (query.includes("_per_capita")) {
+            label = "Singapaore (Tonnes Per Person)";
+        } else if (query.includes("_per_gdp")) {
+            label = "Singapore (KG per dollar of GDP)";
+        } else {
+            label = "Singapore (Million Tonnes)";
+        }
 
-//Read the data
-d3.csv("./owid-co2-data.csv").then(function (data) {
- let sg = [];
+        yData = []
+        for (const valueSg of filteredSG) {
+            yData.push(valueSg[query])
+        }
+        myChart.data.datasets[0].label = label;
+        myChart.data.datasets[0].data = yData;
+        myChart.data.datasets[0].borderColor = color[query];
+        myChart.update();
+        document.getElementById("desc").innerHTML = descSG[query];
+    }
 
- for (let i = 0; i < data.length; i++) {
-   if (data[i].country.toUpperCase() == "SINGAPORE") {
-     sg[data[i].year] = data[i];
-   }
- }
+    //Read the data
+    d3.csv("./owid-co2-data.csv").then(function (data) {
+        let sg = [];
 
- const filteredSG = _.filter(sg, Boolean);
- console.log(filteredSG);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].country.toUpperCase() == "SINGAPORE") {
+                sg[data[i].year] = data[i];
+            }
+        }
+        filteredSG = _.filter(sg, Boolean);
+        const allYear = new Set(filteredSG.map((d) => d.year));
+        // console.log(filteredSG)
 
- //   delete filteredSG[0].cement_co2
- // console.log(Object.keys(filteredSG[1]))
+        var allType = [
+            "co2",
+            "trade_co2",
+            "coal_co2",
+            "gas_co2",
+            "oil_co2",
+            "co2_per_gdp",
+            "co2_per_unit_energy",
+            "consumption_co2",
+            "consumption_co2_per_capita",
+            "consumption_co2_per_gdp",
+            "cumulative_co2",
+            "cumulative_coal_co2",
+            "cumulative_gas_co2",
+            "cumulative_oil_co2",
+            "trade_co2_share",
+            "total_ghg",
+            "ghg_per_capita",
+            "methane",
+            "nitrous_oxide",
+            "energy_per_capita",
+        ];
 
- //   for(let x=0; x<filteredSG.length; x++){
- //     console.log(filteredSG[x].co2)
- //   }
+        var allColors = [
+            "#ff6361",
+            "#58508d",
+            "#bc5090",
+            "#ffa600",
+            "#d45087",
+            "#7DCEA0",
+            "#A9CCE3",
+            "#B7950B",
+            "#873600",
+            "#145A32",
+            "#D7BDE2",
+            "#E59866",
+            "#C39BD3",
+            "#138D75",
+            "#641E16",
+            "#797D7F",
+            "#A2D9CE",
+            "#D68910",
+            "#7D3C98",
+            "#ABEBC6"
+        ];
 
- const allYear = new Set(filteredSG.map((d) => d.year));
- console.log(allYear);
- // var allType = Object.keys(filteredSG[1]);
- var allType = [
-   "co2",
-   "trade_co2",
-   // "cement_co2",
-   "coal_co2",
-   "gas_co2",
-   "oil_co2",
-   "co2_per_gdp",
-   "co2_per_unit_energy",
-   "consumption_co2",
-   "consumption_co2_per_capita",
-   "consumption_co2_per_gdp",
-   "cumulative_co2",
-   "cumulative_coal_co2",
-   "cumulative_gas_co2",
-   "cumulative_oil_co2",
-   "trade_co2_share",
-   "total_ghg",
-   "ghg_per_capita",
-   "methane",
-   "nitrous_oxide",
-   "energy_per_capita",
- ];
+        var counter = 0
+        for (const type of allType) {
+            color[type] = allColors[counter];
+            counter++;
+        }
 
- console.log(allType);
- let selected = "co2";
 
- // add the options to the button
- d3.select("#selectButton")
-   .selectAll("options")
-   .data(allType)
-   .enter()
-   .append("option")
-   .text(function (d) {
-     return d;
-   })
-   .attr("value", function (d) {
-     return d;
-   });
+        yData = []
+        for (const valueSg of filteredSG) {
+            yData.push(valueSg.co2)
+        }
 
- d3.select("#selectButton").on("change", function (event, d) {
-   const selectOption = d3.select(this).property("value");
-   // console.log(selectOption);
-   update(selectOption);
- });
+        for (const cat of allType) {
+            $("#selectButton").append("<option value='" + cat + "'>" + cat + "</option>");
+        }
+        console.log(allYear)
+        console.log(yData)
 
- function update(selectOption) {
-   const dataFilter = filteredSG.filter(function (d) {
-     // console.log(d[selectOption]);
-     selected = selectOption;
-     return d[selectOption];
-   });
+        const lineData = {
+            labels: Array.from(allYear),
+            datasets: [
+                {
+                    label: 'Singapore (Million Tonnes)',
+                    data: yData,
+                    fill: false,
+                    borderColor: '#ff6361',
+                    backgroundColor: 'rgb(255, 99, 132, 0.5)',
+                }
+            ]
+        };
 
-   y.domain([
-     0,
-     d3.max(filteredSG, function (d) {
-       return +d[selectOption];
-     }),
-   ])
-   .range([height1, 0]);
+        myChart = new Chart("lineChart", {
+            type: "line",
+            data: lineData,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            display: true,
+                            autoSkip: true,
+                            maxTicksLimit: 7
+                        }
+                    }]
+                },
+                interaction: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Singapore Carbon Emission'
+                    }
+                }
+            },
+        });
 
-   // svg.append("g").call(d3.axisLeft(y));
-   line
-     .datum(dataFilter)
-     .transition()
-     .duration(1000)
-     .attr(
-       "d",
-       d3
-         .line()
-         .x(function (d) {
-           return x(d.year);
-         })
-         .y(function (d) {
-           return y(+d[selectOption]);
-         })
-     )
-     .attr("stroke", function (d) {
-       return myColor(selectOption);
-     });
- }
-
- var myColor = d3.scaleOrdinal().domain(allType).range(d3.schemeSet2);
-
- var x = d3
-   .scaleLinear()
-   .domain(
-     d3.extent(filteredSG, function (d) {
-       return d.year;
-     })
-   )
-   .range([0, width1]);
- svg1
-   .append("g")
-   .attr("transform", "translate(0," + height1 + ")")
-   .call(d3.axisBottom(x).ticks(7));
-
- // Add Y axis
- var y = d3
-   .scaleLinear()
-   .domain([
-     0,
-     d3.max(filteredSG, function (d) {
-       return +d[selected];
-     }),
-   ])
-   .range([height1, 0]);
- svg1.append("g").call(d3.axisLeft(y));
-
- // Initialize line with first group of the list
- var line = svg1
-   .append("g")
-   .append("path")
-   .datum(
-     filteredSG.filter(function (d) {
-       return allType[0];
-     })
-   )
-   .attr(
-     "d",
-     d3
-       .line()
-       .x(function (d) {
-         return x(d.year);
-       })
-       .y(function (d) {
-         return y(+d[selected]);
-       })
-   )
-   .attr("stroke", function (d) {
-     return myColor("valueA");
-   })
-   .style("stroke-width", 4)
-   .style("fill", "none");
-});
+        //Read the data
+        d3.csv("./owid-co2-codebook.csv").then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                var colName = data[i].column;
+                if (allType.includes(colName)) {
+                    descSG[colName] = data[i].description;
+                }
+            }
+            document.getElementById("desc").innerHTML = descSG['co2'];
+        })
+    });
